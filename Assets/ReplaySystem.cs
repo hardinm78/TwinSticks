506e1b9b0b02c9f46028c4d0b372aff7bf5079b0
hardinm78 <michaelhardin@Michaels-iMac.local>
2017-01-27 14:02:44 -0600

    Replay frame system

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+
+	private Rigidbody rigi;
+
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		Record ();
+	}
+
+	void PlayBack(){
+		rigi.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		print ("reading:" + frame);
+		transform.position = keyFrames [frame].pos;
+		transform.rotation = keyFrames [frame].rot;
+
+	}
+
+
+	void Record ()
+	{
+		rigi.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		print ("writing:" + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+	
+
+}
+
+public struct MyKeyFrame {
+
+	public float time;
+	public Vector3 pos;
+	public Quaternion rot;
+
+	public MyKeyFrame (float time, Vector3 pos, Quaternion rot){
+		this.time = time;
+		this.pos = pos;
+		this.rot = rot;
+	}
+
+
+
+}
